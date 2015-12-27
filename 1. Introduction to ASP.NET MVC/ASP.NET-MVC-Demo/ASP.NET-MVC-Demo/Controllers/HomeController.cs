6f3a42d02b1cb79aed9b5b0c3dca48741f9ca88c
KonstantinKirchev <konstantin1230@yahoo.com>
2015-12-27 10:57:21 +0200

    Lesson 1

---
+++ 1. Introduction to ASP.NET MVC/ASP.NET-MVC-Demo/ASP.NET-MVC-Demo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Demo.Controllers
+{
+    using ASP.NET_MVC_Demo.Models;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kosta()
+        {
+            var obj = new LoginViewModel { Email = "kosta@yahoo.com"};
+            return this.View(obj);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
